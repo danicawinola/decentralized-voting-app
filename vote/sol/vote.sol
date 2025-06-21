144dcb68f2d70f4bb586a6bfcf4dcb828d5248d7
danicawinola <109937301+danicawinola@users.noreply.github.com>
2025-06-21 18:30:07 +0530

    Add files via upload

---
+++ vote/sol/vote.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Voting {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public hasVoted;
+    uint public candidatesCount = 0;
+
+    function addCandidate(string memory _name) public {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!hasVoted[msg.sender], "Already voted");
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate");
+
+        candidates[_candidateId].voteCount++;
+        hasVoted[msg.sender] = true;
+    }
+
+    function getCandidate(uint _id) public view returns (uint, string memory, uint) {
+        Candidate memory c = candidates[_id];
+        return (c.id, c.name, c.voteCount);
+    }
+}
